26128b16cd1ee600b319e7c897b2da2da6cb0edb
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2021-12-03 14:04:47 +0600

    Animation Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer";
-
 import NavMenu from "./components/NavMenu";
 import SmoothScrollbar from "./components/SmoothScrollbar";
-
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,107 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ContactBanner from "../components/ContactBanner";
-
 import HeroSection from "../components/HeroSection";
 import ProjectSection from "../components/ProjectSection";
 import ServiceSection from "../components/ServiceSection";
 import TestimonialsSection from "../components/TestimonialsSection";
+import Particles from "react-tsparticles";
 
-const Home = () => (
-  <div>
-    <HeroSection />
-    <AboutSection />
-    <ServiceSection />
-    <ProjectSection />
-    <TestimonialsSection />
-    <ContactBanner />
-  </div>
-);
+export default function Home() {
+  const particlesInit = (main) => {
+    console.log(main);
 
-export default Home;
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+  return (
+    <div>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <HeroSection />
+      <AboutSection />
+      <ServiceSection />
+      <ProjectSection />
+      <TestimonialsSection />
+      <ContactBanner />
+    </div>
+  );
+}
